5e4d04c7f270d2321345f2b286827e3985018d1d
Yeow Heng <20042741@myrp.edu.sg>
2022-05-06 18:19:29 +0800

    Session 2 completed

--- app/src/main/java/sg/edu/rp/c346/id20042741/l03session2/BillPlease.java
+++ app/src/main/java/sg/edu/rp/c346/id20042741/l03session2/BillPlease.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id20042741.l03session2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -26,6 +27,13 @@ public class BillPlease extends AppCompatActivity {
     Button resetBtn;
     TextView billView;
     TextView splitView;
+    double svsCharge = 0;
+    double gstCharge = 0;
+    boolean cashCheck = false;
+    boolean payNowCheck = false;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +52,51 @@ public class BillPlease extends AppCompatActivity {
         resetBtn = findViewById(R.id.btnReset);
         billView = findViewById(R.id.totalOutputId);
         splitView = findViewById(R.id.splitOutputId);
+
+
+
+        svsToggle.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                svsCharge = svsToggle.isChecked() ? 0.07 : 0;
+                refreshDisplay();
+            }
+        });
+
+        gstToggle.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                gstCharge = gstToggle.isChecked() ? 0.1 : 0;
+                refreshDisplay();
+            }
+        });
+
+        paymentGrp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                payNowCheck = paynowRb.isChecked();
+            }
+        });
+
+        splitBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                refreshDisplay();
+            }
+        });
+
+    }
+    public void refreshDisplay(){
+        double totalAmt = Double.parseDouble(amtTxt.getText().toString());
+        double totalPax = Double.parseDouble(paxTxt.getText().toString());
+        double totalDisc = discTxt.getText().toString().isEmpty() ? 0 : Double.parseDouble(discTxt.getText().toString())/100;
+
+        double paymentDue = totalAmt * (1-(svsCharge + gstCharge + totalDisc));
+        double split = paymentDue / totalPax;
+        String num = payNowCheck ?  "99999999" : "";
+        billView.setText(String.format("Total Bill: %.2f",paymentDue));
+        splitView.setText(String.format("Each Pays: %.2f in cash %s",split,num));
+
 
     }
 }
